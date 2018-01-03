4f871edfd58ca3a0b73d59b6e46e4590daa1cdef
王兵 <emp_kixs@163.com>
2018-01-03 18:36:16 +0800

    zuul 实现路由网关

---
+++ universe-module-zuul/src/test/java/com/universe/zuul/UniverseModuleZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.universe.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UniverseModuleZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
