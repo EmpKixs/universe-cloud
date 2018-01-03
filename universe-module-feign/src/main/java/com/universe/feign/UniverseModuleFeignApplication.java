f16b302fa574d75372219e0838e5ec34f6465955
王兵 <emp_kixs@163.com>
2018-01-03 18:07:28 +0800

    feign和ribbon开启断路器（hystrix）仪表盘

--- universe-module-feign/src/main/java/com/universe/feign/UniverseModuleFeignApplication.java
+++ universe-module-feign/src/main/java/com/universe/feign/UniverseModuleFeignApplication.java
@@ -2,10 +2,14 @@ package com.universe.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
 public class UniverseModuleFeignApplication {
