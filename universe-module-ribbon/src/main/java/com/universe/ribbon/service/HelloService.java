a33e17e1b4f137bd69936ccab58c933634830314
王兵 <emp_kixs@163.com>
2018-01-03 16:51:35 +0800

    在 ribbon 使用断路器（hystrix）

--- universe-module-ribbon/src/main/java/com/universe/ribbon/service/HelloService.java
+++ universe-module-ribbon/src/main/java/com/universe/ribbon/service/HelloService.java
@@ -3,6 +3,7 @@
  */
 package com.universe.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,7 +19,12 @@ public class HelloService {
     @Resource
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://EUREKA-CLIENT-SERVICE/hello/hi?name="+name,String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + "!There has occurred some error!";
     }
 }
